854019b6cdc39a2958493fd2a57a1f8f788a7dce
Anuoluwa <anulamikanra@gmail.com>
2026-07-23 16:57:41 +0100

    I made minor chnages to the code

--- EventManagementSystem/Extensions/SessionExtensions.cs
+++ EventManagementSystem/Extensions/SessionExtensions.cs
@@ -4,23 +4,14 @@ namespace EventManagementSystem.Extensions;
 
 public static class SessionExtensions
 {
-    public static void SetObject<T>(
-        this ISession session,
-        string key,
-        T value)
+    public static void SetObject<T>(this ISession session, string key,  T value)
     {
-        session.SetString(
-            key,
-            JsonSerializer.Serialize(value)
-        );
+        session.SetString(key,JsonSerializer.Serialize(value));
     }
 
-    public static T? GetObject<T>(
-        this ISession session,
-        string key)
+    public static T? GetObject<T>(this ISession session, string key)
     {
-        var json =
-            session.GetString(key);
+        var json = session.GetString(key);
 
         if (string.IsNullOrWhiteSpace(json))
         {
--- EventManagementSystem/Models/AdminBookingViewModel.cs
+++ EventManagementSystem/Models/AdminBookingViewModel.cs
@@ -6,24 +6,19 @@
 
         public int EventId { get; set; }
 
-        public string EventTitle { get; set; } =
-            string.Empty;
+        public string EventTitle { get; set; } = string.Empty;
 
         public int UserId { get; set; }
 
-        public string AttendeeName { get; set; } =
-            string.Empty;
+        public string AttendeeName { get; set; } = string.Empty;
 
-        public string AttendeeEmail { get; set; } =
-            string.Empty;
+        public string AttendeeEmail { get; set; } = string.Empty;
 
         public decimal AmountPaid { get; set; }
 
-        public string BookingStatus { get; set; } =
-            string.Empty;
+        public string BookingStatus { get; set; } =  string.Empty;
 
-        public string PaymentStatus { get; set; } =
-            string.Empty;
+        public string PaymentStatus { get; set; } = string.Empty;
     }
 
 }
